0906c2ead3619a5cc138abcf078cad20d6e6855f
hP ELITEBOOK <koubeeslie17@gmail.com>
2026-06-07 18:31:52 +0000

    projet

--- src/bookstoread/BookShelf.java
+++
@@ -1,8 +0,0 @@
-package bookstoread;
-import java.util.Collections;
-import java.util.List;
-public class BookShelf {
-    public List<String> books() {
-        return Collections.emptyList();
-    }
-}
