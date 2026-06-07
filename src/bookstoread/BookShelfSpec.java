45712926874ef07327dbc5f703eda47d1f0cbf78
hP ELITEBOOK <koubeeslie17@gmail.com>
2026-06-07 19:34:05 +0000

    ajout du troisième test

--- src/bookstoread/BookShelfSpec.java
+++ src/bookstoread/BookShelfSpec.java
@@ -15,16 +15,7 @@ public class BookShelfSpec {
         assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
     }
 
-   /* @Test
-    public void bookshelfContainsTwoBooksWhenTwoBooksAdded() {
-        BookShelf shelf = new BookShelf();
-        shelf.add("Effective Java");
-        shelf.add("Code Complete");
-        List<String> books = shelf.books();
-        assertEquals(2, books.size(), () -> "BookShelf should have two books.");
-    }
 
-    */
 
 
    @Test
@@ -34,4 +25,11 @@ public class BookShelfSpec {
        List<String> books = shelf.books();
        assertEquals(2, books.size(), () -> "BookShelf should have two books.");
    }
+    @Test
+    public void emptyBookShelfWhenAddIsCalledWithoutBooks() {
+        BookShelf shelf = new BookShelf();
+        shelf.add();
+        List<String> books = shelf.books();
+        assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
+    }
 }
