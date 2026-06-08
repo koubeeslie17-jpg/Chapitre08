ace540228797ad6a2ae126fc19475243a6e32211
hP ELITEBOOK <koubeeslie17@gmail.com>
2026-06-08 10:33:38 +0000

    ajout de librairie et du huitième test

--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -1,5 +1,6 @@
 package bookstoread;
 
+import java.time.Year;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -21,5 +22,8 @@ public class BookShelf {
         return books.stream().sorted(criteria).collect(Collectors.toList());
     }
 
-
+    public Map<Year, List<Book>> groupByPublicationYear() {
+        return books.stream().collect(Collectors.groupingBy(book ->
+                Year.of(book.getPublishedOn().getYear())));
+    }
 }
